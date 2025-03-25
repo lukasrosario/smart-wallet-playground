531be69c058388c170a2c57a0a0cdc7286d257d2
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2025-03-25 17:48:32 -0400

    add pm

--- app/api/paymaster/route.ts
+++ app/api/paymaster/route.ts
@@ -1,5 +1,27 @@
+import { createPaymasterClient } from 'viem/account-abstraction';
+import { http } from 'viem';
+
+const paymasterClient = createPaymasterClient({
+  transport: http(process.env.PAYMASTER_URL),
+});
+
 export async function POST(request: Request) {
-  const res = await request.json();
-  console.log('lukas request', res);
-  return Response.json({ res });
+  const req = await request.json();
+  if (req.method === 'pm_getPaymasterStubData') {
+    const userOperation = req.params[0];
+    const entryPointAddress = req.params[1];
+    const chainId = req.params[2];
+    const res = await paymasterClient.getPaymasterStubData({ ...userOperation, entryPointAddress, chainId });
+    console.log('lukas res', res);
+    return res;
+  } else if (req.method === 'pm_getPaymasterData') {
+    const userOperation = req.params[0];
+    const entryPointAddress = req.params[1];
+    const chainId = req.params[2];
+    const res = await paymasterClient.getPaymasterData({ ...userOperation, entryPointAddress, chainId });
+    console.log('lukas res', res);
+    return res;
+  }
+
+  return Response.json({ error: 'Invalid method' }, { status: 400 });
 }
