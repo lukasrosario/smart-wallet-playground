2e4d6f744500a8d12b9d175b5a3fa162027d6441
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2025-03-25 17:53:34 -0400

    logs, fix response shape

--- app/api/paymaster/route.ts
+++ app/api/paymaster/route.ts
@@ -8,19 +8,21 @@ const paymasterClient = createPaymasterClient({
 export async function POST(request: Request) {
   const req = await request.json();
   if (req.method === 'pm_getPaymasterStubData') {
+    console.log('pm_getPaymasterStubData', req);
     const userOperation = req.params[0];
     const entryPointAddress = req.params[1];
     const chainId = req.params[2];
     const res = await paymasterClient.getPaymasterStubData({ ...userOperation, entryPointAddress, chainId });
     console.log('lukas res', res);
-    return Response.json(res);
+    return Response.json({ result: res });
   } else if (req.method === 'pm_getPaymasterData') {
+    console.log('pm_getPaymasterData', req);
     const userOperation = req.params[0];
     const entryPointAddress = req.params[1];
     const chainId = req.params[2];
     const res = await paymasterClient.getPaymasterData({ ...userOperation, entryPointAddress, chainId });
     console.log('lukas res', res);
-    return Response.json(res);
+    return Response.json({ result: res });
   }
 
   return Response.json({ error: 'Invalid method' }, { status: 400 });
