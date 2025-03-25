5113e5d36fba08589527891984be9d46e4f8a7db
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2025-03-25 19:11:27 -0400

    fix chain id mapping

--- app/api/paymaster/client.ts
+++ app/api/paymaster/client.ts
@@ -5,8 +5,8 @@ const CHAIN_ID_TO_PAYMASTER_URL = {
   '0x1': process.env.MAINNET_PAYMASTER_URL,
   '0xa': process.env.OPTIMISM_PAYMASTER_URL,
   '0x2105': process.env.BASE_PAYMASTER_URL,
-  '0xaa36a7': process.env.BASE_SEPOLIA_PAYMASTER_URL,
-  '0x14a34': process.env.SEPOLIA_PAYMASTER_URL,
+  '0x14a34': process.env.BASE_SEPOLIA_PAYMASTER_URL,
+  '0xaa36a7': process.env.SEPOLIA_PAYMASTER_URL,
 } as const;
 
 export function createPaymasterClient(chainId: string) {
