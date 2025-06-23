6a001fe2b9c1451724a835675b6bf8a5620366ba
ibrakhimus <ibrakhimus@gmail.com>
2025-06-23 14:59:05 -0700

    removed unnecessary logs

--- app/api/paymaster/[sponsor]/route.ts
+++ app/api/paymaster/[sponsor]/route.ts
@@ -5,20 +5,10 @@ export async function POST(request: Request, { params }: { params: Promise<{ spo
     const req = await request.json();
     const { sponsor } = await params;
 
-    console.log('=== Paymaster API Request ===');
-    console.log('Method:', req.method);
-    console.log('Sponsor:', sponsor);
-    console.log('Request body:', JSON.stringify(req, null, 2));
-
     if (req.method === 'pm_getPaymasterStubData') {
       const userOperation = req.params[0];
       const entryPointAddress = req.params[1];
       const chainId = req.params[2];
-
-      console.log('pm_getPaymasterStubData params:');
-      console.log('- Chain ID:', chainId);
-      console.log('- Entry Point:', entryPointAddress);
-      console.log('- User Op keys:', Object.keys(userOperation || {}));
 
       try {
         const paymasterClient = createPaymasterClient(chainId);
