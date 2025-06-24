dd60d4bcb51aba1bed82307f23bd87fa30e6b03c
ibrakhimus <randizzys@gmail.com>
2025-06-24 12:29:11 -0700

    redirect to config

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,18 @@
 'use client';
 
-import { ConnectWallet } from './components/ConnectWallet';
-import { EventLog } from './components/EventLog';
-import { SDKConfig } from './components/SDKConfig';
-import { ChainConfig } from './components/ChainConfig';
-import { WAGMIProvider } from './context/WagmiContextProvider';
-import { ConfigProvider } from './context/ConfigContext';
-import { SendUSDC } from './components/SendUSDC';
-import { SendETH } from './components/SendETH';
-import { AppPaymaster } from './components/AppPaymaster';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/config/sdk');
+  }, [router]);
+
   return (
-    <ConfigProvider>
-      <WAGMIProvider>
-        <div className="flex flex-col p-4 md:p-16 md:pt-12 space-y-8 max-w-9xl mx-auto">
-          <ConnectWallet />
-          <EventLog />
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <SDKConfig />
-            <ChainConfig />
-            <SendUSDC />
-            <SendETH />
-            <AppPaymaster />
-          </div>
-        </div>
-      </WAGMIProvider>
-    </ConfigProvider>
+    <div className="flex justify-center items-center min-h-[200px]">
+      <div className="text-white">Redirecting to SDK Config...</div>
+    </div>
   );
 }
